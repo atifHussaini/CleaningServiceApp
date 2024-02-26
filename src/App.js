4d8c490b3b9aed0ee0c751103162a31fcf73f986
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2024-02-26 14:54:43 -0600

    add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{backgroundColor: "wheat"}}>
       <NavBar/>
-      <h1>Maodo's Cleaning and Moving Website</h1>
+      <h1>Maodo's Cleaning and Moving Website!</h1>
     </div>
   );
 }
