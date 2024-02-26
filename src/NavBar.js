a444be5015ee74426e491dad12f8a47979ceddd2
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2024-02-26 14:48:14 -0600

    working on features

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div style={{display: "flex", justifyContent: "space-evenly", color: "blue"}}>
+            <h2 style={{}}>Home</h2>
+            <h2 style={{}}>About</h2>
+            <h2 style={{}}>Contact Us</h2>
+        </div>
+    );
+};
+
+export default NavBar;
